9885f2ed7d3e82ede342856500a169cbd18d8b53
Rodrigo Longhi Guimarães <rguimaraes@alunos.utfpr.edu.br>
2019-10-09 00:35:42 -0300

    Create MovieTableViewCell and show with no data

--- TheVulnerableApp/TheVulnerableApp/ViewController.swift
+++ TheVulnerableApp/TheVulnerableApp/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.register(UINib(nibName: "MovieTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "MovieTableViewCell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 328
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell")!
+    }
 }
 
